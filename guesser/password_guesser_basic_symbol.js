d7f28584f989e423da62af97373867eebeb72db8
rtt10ee <tenneytobias@gmail.com>
2025-09-28 07:10:05 -0400

    Create password_guesser_basic_symbol.js

---
+++ guesser/password_guesser_basic_symbol.js
@@ -0,0 +1,26 @@
+function passwordGuesserBasicSymbols(inputPassword) {
+  const storage = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+  if (inputPassword.length < 3) {
+    console.log("Password must be at least 3 characters long.");
+    return null;
+  }
+  const chars = storage.split('');
+  const length = inputPassword.length;
+  let found = null;
+  function recurse(prefix) {
+    if (prefix.length === length) {
+      if (prefix === inputPassword) {
+        found = prefix;
+        console.log("Password is: " + prefix);
+      }
+      return;
+    }
+    for (const c of chars) {
+      if (found) break;
+      recurse(prefix + c);
+    }
+  }
+  recurse('');
+  if (!found) console.log("Password not found.");
+  return found;
+}
