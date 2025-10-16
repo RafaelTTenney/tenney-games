5f0e0700bc718d0083945fc3867043038f4fcfa7
RafaelTTenney <Rafael.Tenney@outlook.com>
2025-10-16 12:13:02 -0400

    Update hash for user 'smartinez'

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ const users = [
   { username: 'admin', hash: '02079b31824a4d18a105f16b9d45e751a114ce5b4ff3d49c6f19633aed25abbc' },
   { username: 'amagee', hash: 'e52a1359297822655226696b53192f9085c5f161d1bda5cbaed8e9ceb64c904b'},
   { username: 'ccarty', hash: 'e3bd890850be9d6ffc4568c23a497e84fc8ed079ed196ce6d978a24a731f1de8'},
-  { username: 'smartinez', hash: 'e3bd890850be9d6ffc4568c23a497e84fc8ed079ed196ce6d978a24a731f1de8'}
+  { username: 'smartinez', hash: 'cfadedad585d18910973603153c102a1ab83edd78886db527315b07d0630281e'}
 ];
 
 function isLoggedIn() {
