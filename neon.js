4956e08db041af3bf5ad8fb086f64baad212ebf2
RafaelTTenney <Rafael.Tenney@outlook.com>
2025-10-23 12:25:30 -0400

    Create neon.js

---
+++ neon.js
@@ -0,0 +1,20 @@
+Neon green
+{ fill: '#39FF14', stroke: '#00FF00', shadow: '#39FF14' }
+{ fill: '#0FFF50', stroke: '#00E644', shadow: '#0FFF50' }
+{ fill: '#00FF99', stroke: '#00E68A', shadow: '#00FF99' } 
+Neon red
+{ fill: '#FF0000', stroke: '#E60000', shadow: '#FF0000' }
+{ fill: '#FD1C03', stroke: '#E41903', shadow: '#FD1C03' } 
+Neon blue
+{ fill: '#00FFFF', stroke: '#00E6E6', shadow: '#00FFFF' }
+{ fill: '#00CAFF', stroke: '#00B8E6', shadow: '#00CAFF' } 
+Neon pink
+{ fill: '#FF00FF', stroke: '#E600E6', shadow: '#FF00FF' }
+{ fill: '#FF69B4', stroke: '#E6529E', shadow: '#FF69B4' }
+{ fill: '#FF14FC', stroke: '#E60CC7', shadow: '#FF14FC' } 
+Neon orange
+{ fill: '#FF6600', stroke: '#E65C00', shadow: '#FF6600' }
+{ fill: '#FF7E00', stroke: '#E67100', shadow: '#FF7E00' } 
+Neon purple
+{ fill: '#9D00FF', stroke: '#8C00E6', shadow: '#9D00FF' }
+{ fill: '#B915CC', stroke: '#A613B8', shadow: '#B915CC' } 
