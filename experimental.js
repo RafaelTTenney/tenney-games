61dcf4e06348ae27bcb8ec2542285752a660f3f5
RafaelTTenney <Rafael.Tenney@outlook.com>
2025-10-24 06:47:04 -0400

    change shooting speed

--- experimental.js
+++ experimental.js
@@ -821,7 +821,7 @@ function updateInvaders() {
 
   // --- Player Bullet ---
   if (state.bullet.active) {
-    state.bullet.y -= 7;
+    state.bullet.y -= 12;
     if (state.bullet.y < 0) {
       state.bullet.active = false;
       state.bullet.alive = false;
